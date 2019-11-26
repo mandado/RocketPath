63b386d60333a2fb34212ba348a2cc6023378e41
mandado <jorgerobertodev@gmail.com>
2019-11-26 19:05:18 -0300

    fixing layout and created interests page

---
+++ layouts/default.js
@@ -0,0 +1,180 @@
+import '../styles/index.css';
+import { useState, useEffect } from 'react';
+import Modal from '../components/Modal';
+import Input from '../components/Input';
+import Select from '../components/Select';
+import RocketSvg from '../components/icons/Rocket';
+import MountainsSvg from '../components/icons/Mountains';
+import { useRouter } from 'next/router';
+
+import posed from 'react-pose';
+
+
+const Rocket = posed.div({
+  visible: { 
+    x: '120%', 
+    y: -760,
+    transition: {
+      duration: 350,
+      ease: 'linear'
+    } 
+  },
+  hidden: { y: '100%' },
+  content: { 
+    x: '140%', 
+    y: -760,
+    transition: {
+      duration: 350,
+      ease: 'linear'
+    } 
+  }, 
+});
+
+const Mountains = posed.div({
+  visible: { 
+    x: -50, 
+    y: 100,
+    z: 10,
+    transition: {
+      duration: 300,
+      ease: 'linear'
+    } 
+  },
+  hidden: { x: 0, y: '160%' },
+  content: { 
+    x: 300, 
+    y: 100,
+    z: 10,
+    transition: {
+      duration: 300,
+      ease: 'linear'
+    } 
+  }, 
+});
+
+
+function Layout({ children }) {
+  const [modalLogin, setModalLogin] = useState(false);
+  const [modalSignup, setModalSignup] = useState(false);
+  const [logged, setLogged] = useState(false);
+  const [toggled, setToggled] = useState(false);
+  const router = useRouter();
+
+  const loginUser = () => {
+    setModalLogin(false);
+    setLogged(true);
+  };
+
+  const signupUser = () => {
+    setModalSignup(false);
+    setLogged(true);
+  };
+
+  useEffect(() => {
+    setToggled(true);
+  }, []);
+
+  return (
+    <>
+      <nav className="flex navbar p-10 items-center justify-between w-full">
+        <span className="flex w-48 flex-wrap color-brown">
+          <h1 className="text-3xl w-full font-light leading-none">
+            <span className="font-medium">Rocket</span>Path
+          </h1>
+          <h4 className="leading-none text-xl mt-1">
+            <span className="font-light">O seu caminho </span>
+            <span className="font-bold">tech</span> 
+          </h4>
+        </span>
+
+        {
+          logged ? (
+            <div className="flex items-center">
+              <a href="#" className="text-xl mr-4 color-brown">Rank</a>
+              <img src="/images/avatar.png" className="w-20" />
+              <div className="color-brown ml-2">
+                <p className="text-xl font-medium leading-none mb-1">Felipe Ramos</p>
+                <p className="text-md font-light leading-none">
+                  14 challenges
+                </p>
+                <button onClick={() => setLogged(false)} type="button" className="text-sm text-blueteal mt-1 font-light leading-none">
+                  Sair
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div className="flex">
+              <button onClick={() => setModalLogin(true)} className="text-xl color-brown">Entrar</button>
+              <button onClick={() => setModalSignup(true)} className="text-xl color-brown ml-12">Cadastrar</button>
+            </div>
+          )
+        }
+      </nav>
+
+      <div className="content z-10 relative flex flex-wrap justify-center text-center items-center color-brown h-full w-full">
+        {children}
+      </div>
+
+      <Rocket className="rocket absolute" pose={toggled ? (router.pathname !== '/' ? 'content' : 'visible') : 'hidden'}>
+        <RocketSvg />
+      </Rocket>
+      <Mountains className="mountains absolute" pose={toggled ? (router.pathname !== '/' ? 'content' : 'visible') : 'hidden'}>
+        <MountainsSvg />
+      </Mountains>
+        
+      {modalLogin && (
+        <Modal onClose={() => setModalLogin(false)}>
+          <h4 className="color-brown">
+            <span className="text-3xl leading-none mb-2 block">Ei o/</span>
+            <span className="text-xl leading-none">
+              Falta pouco para descobrir <br/> o seu caminho!
+            </span>
+
+            <div className="mt-10 sign-in m-auto">
+              <Input placeholder="EMAIL" />
+              <br/>
+              <Input placeholder="SENHA" />
+              <div className="mt-8 text-center">
+                <button type="button" onClick={loginUser} className="uppercase text-lg bg-blueteal hover:bg-blueteal-ligter text-white font-medium hover:text-white py-1 px-12 mb-8 mt-4 border-4 border-blueteal hover:border-transparent">
+                  Entrar
+                </button>
+              </div>
+            </div>
+          </h4>
+        </Modal>
+      )}
+
+      {modalSignup && (
+        <Modal onClose={() => setModalSignup(false)}>
+          <h4 className="color-brown">
+            <span className="text-3xl leading-none mb-2 block">Ei o/</span>
+            <span className="text-xl leading-none">
+              Falta pouco para descobrir <br/> o seu caminho!
+            </span>
+
+            <div className="mt-10 sign-in m-auto">
+              <Input placeholder="NOME" />
+              <br/>
+              <Input placeholder="EMAIL" />
+              <br/>
+              <Select options={[
+                { value: 'EnsinoFundamental', text: 'Ensino Fundamental' },
+                { value: 'EnsinoMedio', text: 'Ensino Médio' },
+                { value: 'EnsinoSuperior', text: 'Ensino Superior' },
+              ]} />
+              <br/>
+              <Input placeholder="SENHA" />
+              <div className="mt-8 text-center">
+                <button type="button" onClick={signupUser} className="uppercase text-lg bg-blueteal hover:bg-blueteal-ligter text-white font-medium hover:text-white py-1 px-12 mb-8 mt-4 border-4 border-blueteal hover:border-transparent">
+                  Cadastrar
+                </button>
+              </div>
+            </div>
+          </h4>
+        </Modal>
+      )}
+    </>
+  )
+}
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,168 +1,17 @@
-import '../styles/index.css';
-import { useState, useEffect } from 'react';
-import Modal from '../components/Modal';
-import Input from '../components/Input';
-import Select from '../components/Select';
-import RocketSvg from '../components/icons/Rocket';
-import MountainsSvg from '../components/icons/Mountains';
+import React from 'react'
 
-import posed from 'react-pose';
-
-const Rocket = posed.div({
-  visible: { 
-    x: '120%', 
-    y: -760,
-    transition: {
-      duration: 350,
-      ease: 'linear'
-    } 
-  },
-  hidden: { y: '100%' },
-});
-
-const Mountains = posed.div({
-  visible: { 
-    x: -50, 
-    y: 100,
-    z: 10,
-    transition: {
-      duration: 300,
-      ease: 'linear'
-    } 
-  },
-  hidden: { x: 0, y: '160%' }
-});
-
-function Home() {
-  const [modalLogin, setModalLogin] = useState(false);
-  const [modalSignup, setModalSignup] = useState(false);
-  const [logged, setLogged] = useState(false);
-  const [toggled, setToggled] = useState(false);
-
-  const loginUser = () => {
-    setModalLogin(false);
-    setLogged(true);
-  };
-
-  const signupUser = () => {
-    setModalSignup(false);
-    setLogged(true);
-  };
-
-  useEffect(() => {
-    setToggled(true);
-  }, []);
-
+export default function index() {
   return (
-    <>
-      {/* <img src="/images/mountains.svg" /> */}
-      <nav className="flex navbar p-10 items-center justify-between w-full">
-        <span className="flex w-48 flex-wrap color-brown">
-          <h1 className="text-3xl w-full font-light leading-none">
-            <span className="font-medium">Rocket</span>Path
-          </h1>
-          <h4 className="leading-none text-xl mt-1">
-            <span className="font-light">O seu caminho </span>
-            <span className="font-bold">tech</span> 
-          </h4>
-        </span>
+    <div className="pb-64 text-center">
+      <p className="text-4xl font-medium w-full">
+        Descubra e evolua<br/><span className="font-light">seu caminho</span> tech.
+      </p>
 
-        {
-          logged ? (
-            <div className="flex items-center">
-              <img src="/images/avatar.png" className="w-20" />
-              <div className="color-brown ml-2">
-                <p className="text-xl font-medium leading-none mb-1">Felipe Ramos</p>
-                <p className="text-md font-light leading-none">
-                  14 challenges
-                </p>
-                <button onClick={() => setLogged(false)} type="button" className="text-sm text-blueteal mt-1 font-light leading-none">
-                  Sair
-                </button>
-              </div>
-            </div>
-          ) : (
-            <div className="flex">
-              <button onClick={() => setModalLogin(true)} className="text-xl color-brown">Entrar</button>
-              <button onClick={() => setModalSignup(true)} className="text-xl color-brown ml-12">Cadastrar</button>
-            </div>
-          )
-        }
-      </nav>
-
-      <div className="content z-10 relative flex flex-wrap justify-center text-center items-center color-brown h-full w-full">
-        <div className="pb-64 text-center">
-          <p className="text-4xl font-medium w-full">
-            Descubra e evolua<br/><span className="font-light">seu caminho</span> tech.
-          </p>
-
-          <button onClick={() => setModalLogin(true)} className="uppercase text-2xl bg-transparent hover:bg-blueteal text-blueteal font-medium hover:text-white py-2 px-16 mt-8 border-4 border-blueteal hover:border-transparent">
-            Vamos lá
-          </button>
-        </div>
-      </div>
-
-      <Rocket className="rocket absolute" pose={toggled ? 'visible' : 'hidden'}>
-        <RocketSvg />
-      </Rocket>
-      <Mountains className="mountains absolute" pose={toggled ? 'visible' : 'hidden'}>
-        <MountainsSvg />
-      </Mountains>
-      {/* <div className="mountains"></div> */}
-        
-      {modalLogin && (
-        <Modal onClose={() => setModalLogin(false)}>
-          <h4 className="color-brown">
-            <span className="text-3xl leading-none mb-2 block">Ei o/</span>
-            <span className="text-xl leading-none">
-              Falta pouco para descobrir <br/> o seu caminho!
-            </span>
-
-            <div className="mt-10 sign-in m-auto">
-              <Input placeholder="EMAIL" />
-              <br/>
-              <Input placeholder="SENHA" />
-              <div className="mt-8 text-center">
-                <button type="button" onClick={loginUser} className="uppercase text-lg bg-blueteal hover:bg-blueteal-ligter text-white font-medium hover:text-white py-1 px-12 mb-8 mt-4 border-4 border-blueteal hover:border-transparent">
-                  Entrar
-                </button>
-              </div>
-            </div>
-          </h4>
-        </Modal>
-      )}
-
-      {modalSignup && (
-        <Modal onClose={() => setModalSignup(false)}>
-          <h4 className="color-brown">
-            <span className="text-3xl leading-none mb-2 block">Ei o/</span>
-            <span className="text-xl leading-none">
-              Falta pouco para descobrir <br/> o seu caminho!
-            </span>
-
-            <div className="mt-10 sign-in m-auto">
-              <Input placeholder="NOME" />
-              <br/>
-              <Input placeholder="EMAIL" />
-              <br/>
-              <Select options={[
-                { value: 'EnsinoFundamental', text: 'Ensino Fundamental' },
-                { value: 'EnsinoMedio', text: 'Ensino Médio' },
-                { value: 'EnsinoSuperior', text: 'Ensino Superior' },
-              ]} />
-              <br/>
-              <Input placeholder="SENHA" />
-              <div className="mt-8 text-center">
-                <button type="button" onClick={signupUser} className="uppercase text-lg bg-blueteal hover:bg-blueteal-ligter text-white font-medium hover:text-white py-1 px-12 mb-8 mt-4 border-4 border-blueteal hover:border-transparent">
-                  Cadastrar
-                </button>
-              </div>
-            </div>
-          </h4>
-        </Modal>
-      )}
-    </>
+      <button onClick={() => setModalLogin(true)} className="uppercase text-2xl bg-transparent hover:bg-blueteal text-blueteal font-medium hover:text-white py-2 px-16 mt-8 border-4 border-blueteal hover:border-transparent">
+        Vamos lá
+      </button>
+    </div>
   )
 }
 
-export default Home;
+
